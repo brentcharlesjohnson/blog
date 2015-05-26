ec4ddb9f827b2cfd0512bdfb681a2e990462cd75
Brent C. Johnson <brentcharlesjohnson@gmail.com>
2015-05-26 16:39:08 -0700

    changed the production.rb file to fix image load on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+	# Heroku assets fix
+	config.serve_static_assets = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -29,7 +32,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+	# Changed to true to fix background image on Heroku
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
