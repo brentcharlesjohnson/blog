17b3e102baff6d7fcb952922001d061ee1fc4643
Brent C. Johnson <brentcharlesjohnson@gmail.com>
2015-11-02 20:31:11 -0800

    added tests for validation of name and email

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,6 +7,9 @@ RSpec.describe Message do
 																country: "Russia", city: "Ufa", \
 																comment: "Hello World!") }
 
+		let(:message_blank) { Message.new(name: "Daniel", email: "",
+																country: "Russia", city: "Ufa", \
+																comment: "Hello World!") }
 		it "is valid" do 
 			expect(message).to be_valid
 		end
@@ -26,7 +29,38 @@ RSpec.describe Message do
 			expect(message).not_to be_valid
 		end
 
-		it "should update the mailing list if email is present" do :pending
+		it "should accept a valid email address" do
+			valid_addresses = %w[bob@greatwhitenorth.com SUSHI@restaurant.com
+			K_E_N@barbies.pad.org bill+ted@adventure.com] 
+			valid_addresses.each do |v| 
+				message.email = v
+				expect(message).to be_valid, "#{v.inspect} should be valid" 			
+			end 
+		end
+
+		it "should reject invalid addresses" do 
+			invalid_addressess = %w[bob@greatwhitenorth,com SUSHI_restaurant.com
+			ken.boy@barbiespad. bill@ted+adventure.com]
+			invalid_addressess.each do |i|
+				message.email = i
+				expect(message).not_to be_valid "#{i.inspect} should not be valid"
+			end
+		end
+
+		it "should allow duplicate email if blank" do
+			duplicate_message_blank = message_blank.dup
+			message_blank.save
+			expect(duplicate_message_blank).to be_valid
+		end
+
+		it "should have a unique email address" do 
+			duplicate_message = message.dup
+			duplicate_message.email = message.email.upcase
+			message.save
+			expect(duplicate_message).not_to be_valid
+		end
+
+      it "should update the mailing list if email is present" do :pending
 		end
 
 	end
